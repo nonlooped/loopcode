671b58c16fd20165d16b324e62e31c19554fa8e6
Looped <nonlooped@gmail.com>
2026-08-26 11:29:27 +0300

    fix(ui): add image preview close control

--- src/components/ImagePreview.svelte
+++ src/components/ImagePreview.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { IconX } from '@tabler/icons-svelte';
   import { Dialog } from 'bits-ui';
 
   interface Props {
@@ -20,6 +21,9 @@
     <Dialog.Overlay class="modal-overlay image-preview-overlay" />
     <Dialog.Content class="image-preview">
       <Dialog.Title class="visually-hidden-title">{name}</Dialog.Title>
+      <button class="chrome-button image-preview-close" type="button" aria-label="Close image preview" title="Close" onclick={close}>
+        <IconX size={16} stroke={1.55} />
+      </button>
       <img {src} alt={name} />
     </Dialog.Content>
   </Dialog.Portal>
